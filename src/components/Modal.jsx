9ce6c2e47442b851066863ab0d48ddd12070c35f
sreedev-pm <sreedevpm41@gmail.com>
2025-12-10 01:28:47 +0530

    bug fixes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,44 +1,65 @@
 import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Transition
+} from '@headlessui/react';
 import { X } from 'lucide-react';
 
-export default function Modal({ isOpen, onClose, children}) {
+export default function Modal({ isOpen, onClose, children, title }) {
   return (
-    <Transition show={isOpen} as={Fragment}>
-      <Dialog onClose={onClose} className="fixed inset-0 z-50">
-        <div className="min-h-screen flex items-center justify-center">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-30" />
-          </Transition.Child>
+    <Transition as={Fragment} show={isOpen}>
+      <Dialog
+        as="div"
+        className="relative z-50"
+        onClose={onClose}
+      >
+        <DialogBackdrop className="fixed inset-0 bg-black/40 transition-opacity" />
 
-          <Transition.Child
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Transition
             as={Fragment}
-            enter="ease-out duration-300"
+            enter="ease-out duration-200"
             enterFrom="opacity-0 scale-95"
             enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
+            leave="ease-in duration-150"
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
+            show={isOpen}
           >
-            <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <DialogPanel
+              className="
+                w-full max-w-3xl        /* Wider modal for charts */
+                max-h-[90vh]            /* Prevents overflowing past screen */
+                overflow-y-auto          /* Scroll when large */
+                bg-white rounded-xl
+                shadow-2xl p-6
+                relative
+              "
+            >
+              {/* Close Button */}
               <button
                 onClick={onClose}
-                className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
               >
-                <X />
+                <X size={20} />
               </button>
-             
-              {children}
-            </div>
-          </Transition.Child>
+
+              {/* Title */}
+              {title && (
+                <Dialog.Title className="text-xl font-semibold mb-4 pr-8">
+                  {title}
+                </Dialog.Title>
+              )}
+
+              {/* Modal Content */}
+              <div className="w-full">
+                {children}
+              </div>
+
+            </DialogPanel>
+          </Transition>
         </div>
       </Dialog>
     </Transition>
